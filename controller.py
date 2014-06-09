181835571190640ffa0113592c6da8702bdfbf4c
Pachikuna <m.paz.ojeda.r@gmail.com>
2014-06-09 03:01:08 -0400

    version final

--- controller.py
+++ controller.py
@@ -1,31 +1,115 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
 import sqlite3
+
 
 def conectar():
     con = sqlite3.connect('Marcas.db')
     con.row_factory = sqlite3.Row
     return con
 
+
 def obtener_marca():
     con = conectar()
     c = con.cursor()
     query = "SELECT * FROM marca"
-    resultado= c.execute(query)
+    resultado = c.execute(query)
     marca = resultado.fetchall()
     con.close()
     return marca
+
+
+def filtrarProductos(texto):
+    con = conectar()
+    c = con.cursor()
+    query = "SELECT * FROM products WHERE nombre LIKE \"%{0}%\"".format(texto)
+    resultado = c.execute(query)
+    producto = resultado.fetchall()
+    con.close()
+    return producto
+
+
+def insertarproductos(datos):
+    con = conectar()
+    c = con.cursor()
+    query = """INSERT INTO products (codigo,nombre,descripcion,color,
+        preciobruto,precioneto,id_marca) VALUES (?,?,?,?,?,?,?)"""
+    c.execute(query, datos)
+    con.commit()
+
 
 def obtener_productos():
     con = conectar()
     c = con.cursor()
     query = "SELECT * FROM products"
-    resultado= c.execute(query)
+    resultado = c.execute(query)
     producto = resultado.fetchall()
     con.close()
     return producto
 
+
+def obtener_productos2(pk):
+    con = conectar()
+    c = con.cursor()
+    try:
+        query = "SELECT * FROM products WHERE id_marca = ?"
+        resultado = c.execute(query, [pk])
+    except sqlite3.Error as e:
+        exito = False
+        print "Error:", e.args[0]
+    producto = resultado.fetchall()
+    con.close()
+    return producto
+
+
+def buscarEditar(codigo):
+    con = conectar()
+    c = con.cursor()
+    query = "SELECT * FROM products WHERE codigo = ?"
+    resultado = c.execute(query, [codigo])
+    producto = resultado.fetchall()
+    con.commit()
+    con.close()
+    return producto
+
+
+def reescribeProducto(datos, cod2):
+    con = conectar()
+    c = con.cursor()
+    cod = datos[0]
+    nom = datos[1]
+    descrip = datos[2]
+    color = datos[3]
+    preB = datos[4]
+    preN = datos[5]
+    pk = datos[6]
+    query = """UPDATE products SET codigo = ?
+            ,nombre = ?
+            ,color = ?
+            ,descripcion = ?
+            ,preciobruto = ?
+            ,precioneto = ?
+            ,id_marca = ?
+            WHERE codigo = ?"""
+    infonueva = (cod, nom, descrip, color, preB, preN, pk, cod2)
+    c.execute(query, infonueva)
+    con.commit()
+
+
+def delete(codigo):
+    exito = False
+    con = conectar()
+    c = con.cursor()
+    query = "DELETE FROM products WHERE codigo = ?"
+    try:
+        resultado = c.execute(query, [codigo])
+        con.commit()
+        exito = True
+    except sqlite3.Error as e:
+        exito = False
+        print "Error:", e.args[0]
+    con.close()
+    return exito
 
 
 if __name__ == "__main__":
@@ -34,7 +118,6 @@ if __name__ == "__main__":
     for marcas in marca:
         print marcas
 
-
-    producto = obtener_productos()
+    producto = obtener_productos2(2)
     for produc in producto:
         print produc
--- main.py
+++ main.py
@@ -2,31 +2,77 @@
 import sys
 from PySide import QtGui, QtCore
 import controller
-from interfas_Tarea1 import *
+import controller2
+from Producto import *
 
 
 class Form(QtGui.QWidget):
     def __init__(self, parent=None):
         super(Form, self).__init__()
-        self.ventana =  Ui_Tarea1()
+        self.ventana = Ui_Productos()
         self.ventana.setupUi(self)
         self.render_table()
-        self.ventana.pushButton_3.clicked.connect(self.borrar)
         self.cargararchivos()
+        self.ventana.comboBox.activated[int].connect(self.cargararchivos)
+        self.ventana.lineEdit.textChanged[str].connect(self.filtro)
+        self.ventana.pushButton.clicked.connect(self.editar)
+        self.ventana.pushButton_2.clicked.connect(self.abreventana)
+        self.ventana.pushButton_3.clicked.connect(self.borrar)
         self.show()
 
+    def filtro(self, txt):
+        texto = txt.encode('utf8')
+        products = controller.filtrarProductos(txt)
+        self.cargararchivoFiltro(products)
+
+    def abreventana(self):
+        formulario = controller2.Form_2(self)
+        formulario.exec_()
+#        self.cargararchivos()
+
+    def editar(self):
+        model = self.ventana.tabla.model()
+        index = self.ventana.tabla.currentIndex()
+        if index.row() == -1:  # No se ha seleccionado una fila
+            self.errorMessageDialog = QtGui.QErrorMessage(self)
+            self.errorMessageDialog.showMessage("Debe seleccionar una fila")
+            return False
+        else:
+            cod = model.index(index.row(), 0, QtCore.QModelIndex()).data()
+            producto = controller.buscarEditar(cod)
+            self.formulario = controller2.Form_2(self)
+            self.formulario.carga(producto, cod)
+
     def borrar(self):
-        pass
+        model = self.ventana.tabla.model()
+        index = self.ventana.tabla.currentIndex()
+        if index.row() == -1:  # No se ha seleccionado una fila
+            self.errorMessageDialog = QtGui.QErrorMessage(self)
+            self.errorMessageDialog.showMessage("Debe seleccionar una fila")
+            return False
+        else:
+            cod = model.index(index.row(), 0, QtCore.QModelIndex()).data()
+            if (controller.delete(cod)):
+                self.cargararchivos()
+                msgBox = QtGui.QMessageBox()
+                msgBox.setText("EL registro fue eliminado.")
+                msgBox.exec_()
+                return True
+            else:
+                self.ui.errorMessageDialog = QtGui.QErrorMessage(self)
+                self.ui.errorMessageDialog.showMessage("Error al eliminar el registro")
+                return False
 
     def render_table(self):
-
-        self.ventana.TABLA.setSelectionBehavior(
+        self.ventana.tabla.setSelectionBehavior(
             QtGui.QAbstractItemView.SelectRows)
-#        self.ventana.TABLA.setAlternatingRowColors(True)
-#        self.ventana.TABLA.setSortingEnabled(True)
+        self.ventana.tabla.adjustSize()
+        #self.ventana.tabla.setStretch()
 
     def cargararchivos(self):
-        producto = controller.obtener_productos()
+        if (self.ventana.comboBox.currentIndex() == 0):
+            producto = controller.obtener_productos()
+        else: producto = controller.obtener_productos2(self.ventana.comboBox.currentIndex())
         self.model = QtGui.QStandardItemModel(len(producto), 7)
         self.model.setHorizontalHeaderItem(0, QtGui.QStandardItem(u"código"))
         self.model.setHorizontalHeaderItem(1, QtGui.QStandardItem(u"Nombres"))
@@ -52,17 +98,55 @@ class Form(QtGui.QWidget):
             index = self.model.index(r, 6, QtCore.QModelIndex())
             self.model.setData(index, row['id_marca'])
             r = r + 1
-        self.ventana.TABLA.setModel(self.model)
-        self.ventana.TABLA.setColumnWidth(0, 100)
-        self.ventana.TABLA.setColumnWidth(1, 100)
-        self.ventana.TABLA.setColumnWidth(2, 150)
-        self.ventana.TABLA.setColumnWidth(3, 100)
-        self.ventana.TABLA.setColumnWidth(4, 100)
-        self.ventana.TABLA.setColumnWidth(5, 100)
-        self.ventana.TABLA.setColumnWidth(6, 100)
+        self.ventana.tabla.setModel(self.model)
+        self.ventana.tabla.setColumnWidth(0, 100)
+        self.ventana.tabla.setColumnWidth(1, 100)
+        self.ventana.tabla.setColumnWidth(2, 150)
+        self.ventana.tabla.setColumnWidth(3, 100)
+        self.ventana.tabla.setColumnWidth(4, 100)
+        self.ventana.tabla.setColumnWidth(5, 100)
+        self.ventana.tabla.setColumnWidth(6, 100)
+
+    def cargararchivoFiltro(self, productos):
+        self.model = QtGui.QStandardItemModel(len(productos), 7)
+        self.model.setHorizontalHeaderItem(0, QtGui.QStandardItem(u"código"))
+        self.model.setHorizontalHeaderItem(1, QtGui.QStandardItem(u"Nombres"))
+        self.model.setHorizontalHeaderItem(2, QtGui.QStandardItem(u"Descripcion"))
+        self.model.setHorizontalHeaderItem(3, QtGui.QStandardItem(u"Color"))
+        self.model.setHorizontalHeaderItem(4, QtGui.QStandardItem(u"Precio Bruto"))
+        self.model.setHorizontalHeaderItem(5, QtGui.QStandardItem(u"Precio Neto"))
+        self.model.setHorizontalHeaderItem(6, QtGui.QStandardItem(u"id marca"))
+        r = 0
+        for row in productos:
+            index = self.model.index(r, 0, QtCore.QModelIndex())
+            self.model.setData(index, row['codigo'])
+            index = self.model.index(r, 1, QtCore.QModelIndex())
+            self.model.setData(index, row['nombre'])
+            index = self.model.index(r, 2, QtCore.QModelIndex())
+            self.model.setData(index, row['descripcion'])
+            index = self.model.index(r, 3, QtCore.QModelIndex())
+            self.model.setData(index, row['color'])
+            index = self.model.index(r, 4, QtCore.QModelIndex())
+            self.model.setData(index, row['preciobruto'])
+            index = self.model.index(r, 5, QtCore.QModelIndex())
+            self.model.setData(index, row['precioneto'])
+            index = self.model.index(r, 6, QtCore.QModelIndex())
+            self.model.setData(index, row['id_marca'])
+            r = r + 1
+        self.ventana.tabla.setModel(self.model)
+        self.ventana.tabla.setColumnWidth(0, 100)
+        self.ventana.tabla.setColumnWidth(1, 100)
+        self.ventana.tabla.setColumnWidth(2, 150)
+        self.ventana.tabla.setColumnWidth(3, 100)
+        self.ventana.tabla.setColumnWidth(4, 100)
+        self.ventana.tabla.setColumnWidth(5, 100)
+        self.ventana.tabla.setColumnWidth(6, 100)
 
 
 def run():
+    app = QtGui.QApplication(sys.argv)
+    main = Form()
+    sys.exit(app.exec_())
     app = QtGui.QApplication(sys.argv)
     main = Form()
     sys.exit(app.exec_())
